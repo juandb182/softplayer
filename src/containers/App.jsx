5f16903d8f6f65dbecf177a9e2f44e232f91cfa6
Juan Duran <juandb182@gmail.com>
2020-04-13 02:32:36 -0400

    Initial

---
+++ src/containers/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Header from '../components/Header'
+
+
+const App = () => (
+    <div id="app">
+        <Header />
+
+
+    </div>
+
+
+);
+
+
+
+
+
+
+
+export default App;
